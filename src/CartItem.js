6ca1f7d71a2dd8d6585cc8713d5101e169b63e71
CharanOfficials <charanofficials@gmail.com>
2023-09-26 16:09:37 +0530

    Modify subtract state

--- src/CartItem.js
+++ src/CartItem.js
@@ -24,6 +24,17 @@ class CartItem extends React.Component{
             }
         })
     }
+    decreaseQuantity = () => {
+        const { qty } = this.state
+        if (qty === 0) {
+            return
+        }
+        this.setState((prevState) => {
+            return {
+                qty:prevState.qty - 1 
+            }
+        })
+    }
     render() {
         const {price, title, qty, img} = this.state
         return (
@@ -47,6 +58,7 @@ class CartItem extends React.Component{
                             alt='decrease'
                             className='action-icons'
                             src='https://cdn-icons-png.flaticon.com/512/43/43625.png'
+                            onClick={this.decreaseQuantity}
                         />
                         <img
                             alt='delete'
